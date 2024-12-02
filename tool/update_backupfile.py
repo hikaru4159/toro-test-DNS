281627273aa326a3d9a0abe08b73d36b2631a58c
hikaru4159 <hikaru_takahashi@mail.toyota.co.jp>
2024-12-02 14:31:55 +0900

    retry

--- tool/update_backupfile.py
+++ tool/update_backupfile.py
@@ -10,7 +10,7 @@ def convert_yaml(input_file_name, output_file_name):
     yaml_data = yaml.dump(data, default_flow_style=False, allow_unicode=True)
     loaded_data = yaml.safe_load(yaml_data)
 
-    print(loaded_data)
+    print("Loaded Data:", loaded_data)  # デバッグ用の出力
 
     # 新しい形式に変換
     changes = []
@@ -34,10 +34,14 @@ def convert_yaml(input_file_name, output_file_name):
 
     # 最終的な辞書を作成
     output_data = {'Changes': changes}
+    
+    print("Output Data:", output_data)  # デバッグ用の出力
 
     # 新しいYAML形式で出力
     with open(output_file_name, 'w', encoding='utf-8') as outfile:
         yaml.dump(output_data, outfile, allow_unicode=True)
+    
+    print(f"{output_file_name} has been updated.")  # 更新確認の出力
 
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
