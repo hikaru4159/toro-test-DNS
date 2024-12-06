fd76929506eef1f21e50d7576762215bff82bd4f
hikaru4159 <hikaru_takahashi@mail.toyota.co.jp>
2024-12-06 17:44:47 +0900

    jsonファイル名で動作分岐テスト

--- tool/DEL_convert_yaml.py
+++ tool/DEL_convert_yaml.py
@@ -2,18 +2,19 @@ import json
 import yaml
 import sys
 
-def convert_yaml(input_file_name, output_file_name):
+def convert_yaml(input_file_name, output_yaml_file_name, output_json_file_name):
     with open(input_file_name, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
-    yaml_data = yaml.dump(data, default_flow_style=False, allow_unicode=True)
-    loaded_data = yaml.safe_load(yaml_data)
+    # JSONデータを直接YAML形式に変換して出力
+    with open(output_yaml_file_name, 'w', encoding='utf-8') as yaml_outfile:
+        yaml.dump(data, yaml_outfile, default_flow_style=False, allow_unicode=True)
 
-    print("Loaded Data:", loaded_data)  # デバッグ用の出力
+    print(f"{output_yaml_file_name} has been updated.")  # 更新確認の出力
 
     # 新しい形式に変換
     changes = []
-    for record in loaded_data["ResourceRecordSets"]:
+    for record in data["ResourceRecordSets"]:
         if 'AliasTarget' in record:
             changes.append({
                 'Action': 'DELETE',
@@ -32,20 +33,19 @@ def convert_yaml(input_file_name, output_file_name):
 
     # 最終的な辞書を作成
     output_data = {'Changes': changes}
-    
-    print("Output Data:", output_data)  # デバッグ用の出力
 
-    # 新しいYAML形式で出力
-    with open(output_file_name, 'w', encoding='utf-8') as outfile:
-        yaml.dump(output_data, outfile, allow_unicode=True)
-    
-    print(f"{output_file_name} has been deleted.")  # 更新確認の出力
+    # JSON形式で出力
+    with open(output_json_file_name, 'w', encoding='utf-8') as json_outfile:
+        json.dump(output_data, json_outfile, indent=2)
+
+    print(f"{output_json_file_name} has been created.")  # 更新確認の出力
 
     # YAMLファイルの内容を標準出力に出力（GitHub Actionsで取得できるように）
     print("---")
-    print(yaml.dump(output_data, allow_unicode=True))
+    print(yaml.dump(data, allow_unicode=True))  # 元のデータのYAML出力
 
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
-    output_file_name = sys.argv[2]
-    convert_yaml(input_file_name, output_file_name)
+    output_yaml_file_name = sys.argv[2]
+    output_json_file_name = sys.argv[3]
+    convert_yaml(input_file_name, output_yaml_file_name, output_json_file_name)
