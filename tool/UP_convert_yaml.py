d3ba8deba77a30cf4cac208f67cff7d3215b5ac3
hikaru4159 <hikaru_takahashi@mail.toyota.co.jp>
2024-12-12 11:04:41 +0900

    除外レコードの厳格化

--- tool/UP_convert_yaml.py
+++ tool/UP_convert_yaml.py
@@ -2,10 +2,11 @@ import json
 import yaml
 import sys
 
-def convert_record_sets_to_changes(record_sets, action):
+def convert_record_sets_to_changes(record_sets, action, zone_name):
     changes = []
     for record in record_sets:
-        if record['Type'] in ['SOA', 'NS']:
+        # SOAレコードとNSレコードを除外する条件を厳密にする
+        if record['Type'] in ['SOA', 'NS'] and record['Name'] == zone_name:
             continue
 
         changes.append({
@@ -24,7 +25,7 @@ def write_json_file(filename, data):
         json.dump(data, json_file, indent=2)
     print(f"{filename} has been created/updated.")
 
-def convert_yaml(input_file_name, output_yaml_file_name, output_DEL_json_name, output_UP_json_name):
+def convert_yaml(input_file_name, output_yaml_file_name, output_DEL_json_name, output_UP_json_name, zone_name):
     reference_yaml_file = "maasapis.com/maasapis-com.yaml"
     with open(reference_yaml_file, 'r', encoding='utf-8') as ref_file:
         reference_data = yaml.safe_load(ref_file)
@@ -33,14 +34,14 @@ def convert_yaml(input_file_name, output_yaml_file_name, output_DEL_json_name, o
         data = json.load(json_file)
 
     # DELETE changes
-    delete_changes = convert_record_sets_to_changes(reference_data["ResourceRecordSets"], 'DELETE')
+    delete_changes = convert_record_sets_to_changes(reference_data["ResourceRecordSets"], 'DELETE', zone_name)
     if delete_changes:
         write_json_file(output_DEL_json_name, {'Changes': delete_changes})
     else:
         print("No changes for DELETE action.")
 
     # UPSERT changes
-    changes = convert_record_sets_to_changes(data["ResourceRecordSets"], 'UPSERT')
+    changes = convert_record_sets_to_changes(data["ResourceRecordSets"], 'UPSERT', zone_name)
     if not all(record['Type'] in ['NS', 'SOA'] for record in data["ResourceRecordSets"]):
         write_json_file(output_UP_json_name, {'Changes': changes})
 
@@ -54,8 +55,8 @@ def convert_yaml(input_file_name, output_yaml_file_name, output_DEL_json_name, o
     print(yaml.dump(data, allow_unicode=True))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: script.py <input_json> <output_yaml> <output_del_json> <output_up_json>")
+    if len(sys.argv) != 6:
+        print("Usage: script.py <input_json> <output_yaml> <output_del_json> <output_up_json> <zone_name>")
         sys.exit(1)
 
     convert_yaml(*sys.argv[1:])
